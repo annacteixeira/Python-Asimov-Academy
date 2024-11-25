6b58bfe107b3483ca54deca8ad5bc37cfe1ea3c5
annacteixeira <20202003920@aluno.cefetmg.br>
2024-11-25 14:08:58 -0300

    módulo 01 - jogo dos estados

--- Modulo 01 - Conceitos Basicos/04 - Dicionarios e Metodos/05_desafio_jogo_estados.py
+++ Modulo 01 - Conceitos Basicos/04 - Dicionarios e Metodos/05_desafio_jogo_estados.py
@@ -1,3 +1,11 @@
+# Crie um "jogo dos estados". Neste jogo, o jogador precisa responder o nome da capital
+# de cada estado do Brasil. O jogo deve perguntar ao usuário "Qual a capital do Estado X?"
+# e checar se o usuário respondeu de forma correta. Após cada pergunta, o usuário
+# pode escolher parar o jogo ou continuar para a próxima pergunta. 
+
+# Quando o usuário decidir parar, ou quanto todas as perguntas forem respondidas,
+# o código mostra o número bruto e a porcentagem de acertos
+
 import random
 
 capitais = {
@@ -41,22 +49,22 @@ print('Você precisa responder o nome da capital de cada estado do Brasil.')
 print('=' * 80)
 
 for estado in estados:
-    resposta = input(f"Qual a capital do estado {estado}? ").strip()
+    resposta = input(f'Qual a capital do estado {estado}? ').strip()
     if resposta.lower() == capitais[estado].lower():
-        print("Resposta correta!")
+        print('Resposta correta!\n')
         acertos += 1
     else:
-        print(f"Resposta errada! A capital de {estado} é {capitais[estado]}.")
+        print(f'Resposta errada! A capital de {estado} é {capitais[estado]}.\n')
     
     total += 1
     
-    continuar = input("Você deseja continuar? [S/N]: ").strip().lower()
+    continuar = input('Você deseja continuar? [S/N]: ').strip().lower()
     if continuar == 'n':
         break
 
 print('=' * 80)
-print(f"Você respondeu {total} perguntas.")
-print(f"Acertos: {acertos}")
-print(f"Porcentagem de acertos: {acertos / total * 100:.2f}%")
-print("Obrigada por jogar!")
+print(f'Você respondeu {total} perguntas.')
+print(f'Acertos: {acertos}')
+print(f'Porcentagem de acertos: {acertos / total * 100:.2f}%')
+print('Obrigada por jogar!')
 print('=' * 80)
