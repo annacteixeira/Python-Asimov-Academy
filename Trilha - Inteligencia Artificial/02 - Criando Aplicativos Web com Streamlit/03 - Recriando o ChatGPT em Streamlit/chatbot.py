8850a9aec357052aaa651b3c19cc568f550c4436
annacteixeira <20202003920@aluno.cefetmg.br>
2024-12-23 11:59:44 -0300

    Salvando as conversas em arquivos

--- Trilha - Inteligencia Artificial/02 - Criando Aplicativos Web com Streamlit/03 - Recriando o ChatGPT em Streamlit/chatbot.py
+++ Trilha - Inteligencia Artificial/02 - Criando Aplicativos Web com Streamlit/03 - Recriando o ChatGPT em Streamlit/chatbot.py
@@ -1,5 +1,12 @@
+import re
 import streamlit as st
 import openai
+from unidecode import unidecode
+from pathlib import Path
+import pickle
+
+PASTA_MENSAGENS = Path(__file__).parent / 'mensagens'
+PASTA_MENSAGENS.mkdir(exist_ok=True)
 
 openai_key = 'sua api key aqui'
 
@@ -18,12 +25,47 @@ def retorna_resposta_modelo(mensagens,
     
     return response
 
+def converte_nome_mensagem(nome_mensagem):
+    nome_arquivo = unidecode(nome_mensagem)
+    nome_arquivo = re.sub('\W+', '', nome_arquivo).lower()
+    return nome_arquivo
+
+def retorna_nome_da_mensagem(mensagens):
+    nome_mensagem = ''
+    for mensagem in mensagens:
+        if mensagem['role'] == 'user':
+            nome_mensagem += mensagem['content'][:30]
+            break
+    return nome_mensagem
+
+def salvar_mensagens(mensagens):
+    if len(mensagens) == 0:
+        return False
+    nome_mensagem = retorna_nome_da_mensagem(mensagens)
+    nome_arquivo = converte_nome_mensagem(nome_mensagem)
+    arquivo_salvar = {'nome_mensagem':nome_mensagem, 
+                      'nome_arquivo':nome_arquivo,
+                      'mensagem':mensagens}
+    
+    with open(PASTA_MENSAGENS / nome_arquivo, 'wb') as f:
+        pickle.dump(arquivo_salvar, f)
+
+def ler_mensagens(mensagens, key='mensagem'):
+    if len(mensagens) == 0:
+        return []
+    nome_mensagem = retorna_nome_da_mensagem(mensagens)
+    nome_arquivo = converte_nome_mensagem(nome_mensagem)
+    
+    with open(PASTA_MENSAGENS / nome_arquivo, 'rb') as f:
+        mensagens = pickle.load(f)
+    return mensagens[key]
+
 def pagina_principal():
     
     if not 'mensagens' in st.session_state:
         st.session_state.mensagens = []
         
-    mensagens = st.session_state['mensagens']
+    mensagens = ler_mensagens(st.session_state['mensagens'])
         
     st.header('🤖 Chatbot', divider=True)
 
@@ -57,5 +99,6 @@ def pagina_principal():
         
         
         st.session_state['mensagens'] = mensagens
+        salvar_mensagens(mensagens)
         
 pagina_principal()
