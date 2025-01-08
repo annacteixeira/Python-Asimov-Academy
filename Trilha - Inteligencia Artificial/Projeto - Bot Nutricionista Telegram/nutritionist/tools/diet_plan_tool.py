a184db76445ccf1ea7816b149cc23716c112d238
Anna Chandra <annaftteixeira@gmail.com>
2025-01-08 09:55:54 -0300

    Construindo as Ferramentas do Agente Nutricionista

---
+++ Trilha - Inteligencia Artificial/Projeto - Bot Nutricionista Telegram/nutritionist/tools/diet_plan_tool.py
@@ -0,0 +1,40 @@
+from langchain.tools import BaseTool
+from typing import Optional
+from nutritionist.repositories.diet_plan import DietPlanRepository
+from nutritionist.repositories.user import UserRepository
+
+class DietPlanTool(BaseTool):
+    name: str = 'diet_plan'
+    description: str = (
+        "Use esta ferramenta para criar um plano de dieta de um usuário. "
+        "Entrada: telegram_id do usuário e, e plan_details para criar um novo plano ou buscar um plano já existente."
+        "A regra para essa Tool é quando o usuario gostar do plano montado por você ai você está autorizado a usar essa tool para salvar o plano"
+    )
+    
+    def __init__(self, ):
+        super().__init__()
+        self._user_repo = UserRepository()
+        self._diet_plan_repo = DietPlanRepository()
+        
+    def _run(self, telegram_id: int, plan_details: str = None) -> str:
+        try:
+            user = self._user_repo.get_user_by_telegram_id(telegram_id)
+            if not user:
+                return 'Usuário não encontrado. Por favor, registre o usuário antes de criar um plano de dieta.'
+            
+            if plan_details:
+                self._diet_plan_repo.create_diet_plan(user.telegram_id, plan_details)
+                return f'Plano de dieta criado com sucesso para o usuário {user.name}.'
+            
+            latest_plan = self._diet_plan_repo.get_latest_diet_plan_for_user(user.telegram_id)
+            if latest_plan:
+                return f'Plano de dieta para {user.name}:\n{latest_plan.details}'
+            else:
+                return f'Nenhum plano de dieta encontrado para o usuário {user.name}.'
+        except Exception as e:
+            return f'Erro na ferramenta do plano de dieta: {str(e)}'
+        
+        
+    
+    async def _arun(self, plan_details: Optional[str] = None) -> str:
+        raise NotImplementedError('Execução assíncrona não suportada.')
---
+++ Trilha - Inteligencia Artificial/Projeto - Bot Nutricionista Telegram/nutritionist/tools/user_registration_tool.py
@@ -0,0 +1,62 @@
+from typing import Any, Dict, Type
+from pydantic import BaseModel
+from langchain.tools import BaseTool
+from nutritionist.repositories.user import UserRepository
+from nutritionist.models import User
+
+class UserRegistrationTool(BaseTool):
+    name: str = 'user_registration'
+    
+    description: str = (
+    "Use esta ferramenta para registrar um novo usuário ou atualizar as informações de um usuário existente. "
+    "Esta ferramenta requer os seguintes dados do usuário: "
+    "name (nome), gender (gênero), age (idade como uma string), height_cm (altura em centímetros como uma string), weight_kg (peso em quilogramas como uma string), "
+    "has_diabetes (se tem diabetes: sim/não) e goal (objetivo: perder peso, ganhar peso, ganhar massa muscular). "
+    "Se algum dado estiver faltando, você deve primeiro coletar essas informações do usuário antes de usar esta ferramenta."
+    )
+    
+    args_schema: Type[BaseModel] = User
+    
+    def __init__(self):
+        super().__init__()
+        self._user_repo = UserRepository()
+        
+    def _run(self,
+        telegram_id: int,
+        name: str,
+        gender: str,
+        age: str,
+        height_cm: str,
+        weight_kg: str,
+        has_diabetes: str,
+        goal: str        
+    ) -> str:
+        if not name:
+            raise AttributeError('Os atributos inseridos na Tool UserRegistration não podem ser vazios')
+        
+        try:
+            user_data = {
+                'telegram_id': telegram_id,
+                'name': name,
+                'gender': gender,
+                'age': age,
+                'height_cm': height_cm,
+                'weight_kg': weight_kg,
+                'has_diabetes': has_diabetes,
+                'goal': goal
+            }
+            
+            user = self._user_repo.get_user_by_telegram_id(telegram_id)
+            if user:
+                updated_user = self._user_repo.update_user(**user_data)
+                return f'Informações atualizadas com sucesso para {updated_user.name}.'
+            
+            new_user = self._user_repo.create_user(**user_data)
+            return f'Usuário {new_user.name} registrado com sucesso.'
+        
+        except Exception as err:
+            return f'Erro ao registrar o usuário: {str(err)}'
+    
+    async def _arun(self, user_data: Dict[str, Any]) -> str:
+        raise NotImplementedError('Execução assíncrona não suportada.')
+    
